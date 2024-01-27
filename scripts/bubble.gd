00ac27619466b5d69a2f47437708c492fbf1bff9
Eric Gadbin <eric.gadbin@epitech.eu>
2024-01-27 20:00:31 +0100

    EG:code: ajout de couleurs

--- scripts/bubble.gd
+++ scripts/bubble.gd
@@ -14,24 +14,24 @@ func _on_pressed():
 func connect_to_parent(parent, given_color):
 	if (given_color == 0) :
 		self.text = "Y e a E"
-		self.add_theme_color_override("font_color", Color(255,0,0,2))
+		self.add_theme_color_override("font_color", Color("#ba65c9"))
 		var new_stylebox_normal = self.get_theme_stylebox("normal").duplicate()
 		new_stylebox_normal.border_width_top = 3
-		new_stylebox_normal.border_color = Color(255, 0, 0)
+		new_stylebox_normal.border_color = Color("#ba65c9")
 		self.add_theme_stylebox_override("normal", new_stylebox_normal)
 	if (given_color == 1) :
 		self.text = "m V N n"
-		self.add_theme_color_override("font_color", Color(0,255,0,2))
+		self.add_theme_color_override("font_color", Color("#85ba46"))
 		var new_stylebox_normal = self.get_theme_stylebox("normal").duplicate()
 		new_stylebox_normal.border_width_top = 3
-		new_stylebox_normal.border_color = Color(0, 255, 0)
+		new_stylebox_normal.border_color = Color("#85ba46")
 		self.add_theme_stylebox_override("normal", new_stylebox_normal)
 	if (given_color == 2) :
 		self.text = "q Z s Q"
-		self.add_theme_color_override("font_color", Color(0,0,255,2))
+		self.add_theme_color_override("font_color",  Color("#56a7be"))
 		var new_stylebox_normal = self.get_theme_stylebox("normal").duplicate()
 		new_stylebox_normal.border_width_top = 3
-		new_stylebox_normal.border_color = Color(0, 0, 255)
+		new_stylebox_normal.border_color = Color("#56a7be")
 		self.add_theme_stylebox_override("normal", new_stylebox_normal)
 	if (parent) :
 		self.player_joke.connect(parent._on_bubble_player_joke)
