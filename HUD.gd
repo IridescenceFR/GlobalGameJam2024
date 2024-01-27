7e45d8eb04f6d281c8fd52d10ca65f83f7dba613
Eric Gadbin <eric.gadbin@epitech.eu>
2024-01-27 01:29:27 +0100

    EG:code: ajout d'un boutton quit

--- HUD.gd
+++ HUD.gd
@@ -2,3 +2,14 @@ extends CanvasLayer
 
 # Notifies `Main` node that the button has been pressed
 signal start_game
+
+
+func _on_start_button_pressed():
+	$StartButton.hide()
+	start_game.emit()
+	pass # Replace with function body.
+
+
+func _on_quit_pressed():
+	get_tree().quit()
+	pass # Replace with function body.
