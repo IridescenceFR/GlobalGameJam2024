1b67c17cd8de33be07ec932d196faca9e21696c8
Emma <emma.rouzaud@epitech.eu>
2024-01-27 17:57:11 +0100

    bulles dans main

--- scripts/bubble.gd
+++ scripts/bubble.gd
@@ -3,18 +3,11 @@ extends Button
 signal player_joke(color: int, time_left: float)
 
 var color:int = 0
+var has_parent:bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
-func _on_show_joke_timeout():
-	visible = true
-	$hideJoke.start()
 
 func _on_hide_joke_timeout():
 	queue_free()
@@ -24,5 +17,6 @@ func _on_pressed():
 	player_joke.emit(color, $hideJoke.get_time_left())
 	queue_free()
 
-func _on_send_parent(parent):
-	self.player_joke.connect(parent._on_bubble_player_joke)
+func connect_to_parent(parent):
+	if (parent) :
+		self.player_joke.connect(parent._on_bubble_player_joke)
