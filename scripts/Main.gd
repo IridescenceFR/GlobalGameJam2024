f3d400e2fb5d3a87dabd6390aa4a7d44be1f5743
Eric Gadbin <eric.gadbin@epitech.eu>
2024-01-28 17:44:53 +0100

    EG:code: replacement du boutton

--- scripts/Main.gd
+++ scripts/Main.gd
@@ -35,12 +35,12 @@ func _on_hud_start_game():
 func spawn_bubble():
 	randomize()
 	color_list.shuffle()
-	var pos_x = 0
+	var pos_x = -186
 	for color in color_list:
 		var bubble_scene: PackedScene = preload("res://Scenes/bubble.tscn")
 		var bubble = bubble_scene.instantiate()
 		bubble.color = color
-		bubble.position = Vector2(560 + pos_x, 150)
+		bubble.position = Vector2(560 + pos_x, 100)
 		bubble.connect_to_parent(self, color)
 		bubble.add_to_group("bubbles")
 		add_child(bubble)
